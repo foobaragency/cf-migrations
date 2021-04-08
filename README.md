3e3298cca3c8cc847b7925317637cfdba2370459
Vinicius Marques <vini.type@gmail.com>
2021-04-08 14:25:55 +0200

    chore: add initial scripts and dependencies

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">
+  Contentful Migrations Manager
+</h1>
+
+<p align="center">
+  Manage Contentful migration workflow.
+</p>
+
+## Usage
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleFileExtensions: ["js", "json", "ts"],
+  testRegex: ".test.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
+}
