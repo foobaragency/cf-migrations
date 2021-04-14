c533003e0a5740183446c41bda01d05dfecad9ec
Vinicius Marques <vinicius@foobar.agency>
2021-04-14 14:35:47 +0200

    test(contentful): add management test

---
+++ test/contentful/management.test.ts
@@ -0,0 +1,29 @@
+import { CollectionProp } from "contentful-management/dist/typings/common-types"
+import { EntryProps } from "contentful-management/dist/typings/export-types"
+
+import { getDeployedMigrationNames } from "../../lib/contentful/management"
+
+describe("Contentful Management", () => {
+  const migrationEntries = ({
+    items: [
+      {
+        fields: { fileName: { "en-US": "0004-migration.ts" } },
+      },
+      {
+        fields: { fileName: { "en-US": "0003-migration.ts" } },
+      },
+      {
+        fields: { fileName: { "en-US": "0002-migration.ts" } },
+      },
+      {
+        fields: { fileName: { "en-US": "0001-migration.ts" } },
+      },
+    ],
+  } as unknown) as CollectionProp<EntryProps>
+
+  it("extracts deployed migration names from the Migration entries", () => {
+    const deployedMigrations = getDeployedMigrationNames(migrationEntries)
+
+    expect(deployedMigrations).toMatchSnapshot()
+  })
+})
