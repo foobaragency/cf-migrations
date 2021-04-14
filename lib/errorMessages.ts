f8f5231ca8d3667ff79e87b109fd5febebca0e60
Vinicius Marques <vinicius@foobar.agency>
2021-04-14 14:46:00 +0200

    style(errorMessages): add missing end point to the sentences

--- lib/errorMessages.ts
+++ lib/errorMessages.ts
@@ -1,7 +1,7 @@
 export const errorMessages = {
   migration: {
     invalidNameFormat: (file: string) =>
-      `Invalid migration name format: ${file}. Unable to determine the migration sequence`,
+      `Invalid migration name format: ${file}. Unable to determine the migration sequence.`,
     sequenceGap: (file: string) =>
       `There's a migration gap before the file ${file}. Missing migration files.`,
     duplicatedSequence: (file: string) =>
