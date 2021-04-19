f6234d0adf19b3f2d567acc5ee0cd1db5ef03a85
Vinicius Marques <vinicius@foobar.agency>
2021-04-19 11:41:04 +0200

    refactor(init-environment): remove process.cwd and use path.resolve

--- lib/contentful/initEnvironment.ts
+++ lib/contentful/initEnvironment.ts
@@ -5,8 +5,8 @@ import { runMigration } from "contentful-migration"
 import { ContentfulPartialOptions } from "../types"
 
 export async function initEnvironment(options: ContentfulPartialOptions) {
-  const filePath = path.normalize(
-    `${process.cwd()}/node_modules/cf-migrations/dist/migrations/0001-create-migration-type.js`
+  const filePath = path.resolve(
+    "node_modules/cf-migrations/dist/migrations/0001-create-migration-type.js"
   )
   await runMigration({
     ...options,
