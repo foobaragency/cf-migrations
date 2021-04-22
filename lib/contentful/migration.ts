dbfb85ead954c42bfe8a4c32ba5fd1b43da8d863
Vinicius Marques <vinicius@foobar.agency>
2021-04-22 13:18:09 +0200

    fix(contentful-migration): set true as `yes` default value

--- lib/contentful/migration.ts
+++ lib/contentful/migration.ts
@@ -6,7 +6,7 @@ export async function runMigrations(
   migrationFilePaths: string[],
   options: MigrationOptions
 ) {
-  const yes = options.yes
+  const yes = options.yes !== undefined ? options.yes : true
   for (const filePath of migrationFilePaths) {
     await runMigration({ ...options, filePath, yes })
   }
