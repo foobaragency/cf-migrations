b9951d8f82a53aff5daf62cd3a9656780fb865af
Vinicius Marques <vinicius@foobar.agency>
2021-04-22 15:36:41 +0200

    feature(cli-release): add release command

---
+++ lib/cli/commands/release.ts
@@ -0,0 +1,71 @@
+import { Argv } from "yargs"
+
+import {
+  createDeploymentRelease,
+  ReleaseOptions,
+} from "../../releaseDeployment"
+import { executeHandler } from "../executeHandler"
+import { info, success } from "../logger"
+import {
+  ContentfulCredentialArgs,
+  contentfulCredentialOptions,
+} from "../options/contentful-credentials"
+import { migrationsPathOptions } from "../options/migrations"
+
+type ReleaseArgs = ContentfulCredentialArgs & {
+  migrationsPath: string
+  prefix: string
+  availableEnvironments: number
+  ignoreMigrationCheck?: boolean
+}
+
+export const desc = "Deploy migrations"
+
+export const builder = (yargs: Argv<{}>) =>
+  contentfulCredentialOptions(migrationsPathOptions(yargs))
+    .option("prefix", {
+      alias: ["cf-release-prefix", "rp"],
+      type: "string",
+      description: "Prefix of the release environments",
+    })
+    .option("availableEnvironments", {
+      alias: ["contentful-available-environments", "me"],
+      type: "number",
+      description: "Number of available Contentful Environments",
+    })
+    .option("ignoreMigrationCheck", {
+      alias: ["i"],
+      type: "boolean",
+      description: "Ignore pending migration check before deploying",
+    })
+    .demandOption(["prefix", "availableEnvironments"])
+
+export const handler = async (args: ReleaseArgs) => {
+  await executeHandler(async () => {
+    const releaseOptions = getReleaseOptions(args)
+    const releaseEnvironmentId = await createDeploymentRelease(releaseOptions)
+
+    if (releaseEnvironmentId) {
+      success(
+        `A release ${releaseEnvironmentId} for the environment ${args.env} space ${args.space}`
+      )
+    } else {
+      info(`Skipping release since no pending migration was found`)
+    }
+  })
+}
+
+function getReleaseOptions(args: ReleaseArgs): ReleaseOptions {
+  return {
+    releasePrefix: args.prefix,
+    availableEnvironments: args.availableEnvironments,
+    ignoreMigrationCheck: args.ignoreMigrationCheck,
+    options: {
+      accessToken: args.token,
+      environmentId: args.env,
+      spaceId: args.space,
+      migrationsDirectory: args.migrationsPath,
+      locale: args.locale,
+    },
+  }
+}
