73528099e7ae087fd6d0900c02202cb17a45bf97
Peter Mbanugo <peter@foobar.agency>
2021-07-02 11:57:09 +0200

    fix: don't save migrations that were aborted

--- lib/contentful/migration.ts
+++ lib/contentful/migration.ts
@@ -3,14 +3,24 @@ import { runMigration } from "contentful-migration"
 import { info, success } from "../logger"
 import { MigrationOptions, PendingMigration } from "../types"
 
+export type MigrationResult = { successful: boolean, fileName: string }
+
 export async function runMigrations(
   pendingMigrations: PendingMigration[],
   options: MigrationOptions
-) {
+): Promise<MigrationResult[]> {
   const yes = options.yes !== undefined ? options.yes : true
-  for (const { filePath } of pendingMigrations) {
+  const migrationResult: MigrationResult[] = []
+  for (const { filePath, fileName } of pendingMigrations) {
     info(`Deploying migration ${filePath}...`)
-    await runMigration({ ...options, filePath, yes })
-    success(`${filePath} migration deployed`)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const result = await runMigration({ ...options, filePath, yes })
+    if (result) {
+      success(`${filePath} migration deployed`)
+      migrationResult.push({ successful: true, fileName })
+    }
+    migrationResult.push({ successful: false, fileName })
   }
+
+  return migrationResult
 }
