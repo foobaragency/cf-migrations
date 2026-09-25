25d9b762a7715b028f63eba45ed37de3cf4e80d1
Waqas Noor <waqasn2c@gmail.com>
2026-09-25 13:17:44 +0200

    fix: typescript fixing typescript for new commands

--- lib/cli/index.ts
+++ lib/cli/index.ts
@@ -1,4 +1,4 @@
-import yargs from "yargs"
+import yargs, { CommandModule } from "yargs"
 import { config as loadEnv } from "dotenv"
 import { hideBin } from "yargs/helpers"
 
@@ -10,10 +10,16 @@ import * as copyWorkflows from "./commands/copy-workflows"
 
 loadEnv()
 
-const commands = [create, deploy, init, release, copyWorkflows]
+const commands = [
+  create,
+  deploy,
+  init,
+  release,
+  copyWorkflows,
+] as unknown as CommandModule[]
 
-export default commands
-  .reduce((cli, command) => cli.command(command), yargs(hideBin(process.argv)))
+export default yargs(hideBin(process.argv))
+  .command(commands)
   .demandCommand(1, "You need to specify a command")
   .help("h")
   .alias("h", "help").argv
